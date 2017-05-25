be4ba0b7e9e62be0dbdf8ca00b6fd06a31d57f69
Ricshardson <ricshardson@gmail.com>
2017-05-25 13:49:51 +0200

    baszkikologoltam

--- app.py
+++ app.py
@@ -2,9 +2,21 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 app.debug = True
+#az index fugvnveny rendeleli le az index.html file-t
+
+
 @app.route("/")
-def hello():
-	return render_template('index.html')
+def index():
+	user = "Ricshardson"
+	#a user változó, az az index.html file-ban is user változóként lesz elérhető
+	return render_template('index.html', user=user)
+
+
+
+
+@app.route("/login")
+def loginPage():
+	return render_template('login.html')
 
 if __name__ == "__main__":
 	 app.run(host='0.0.0.0', debug=True)
